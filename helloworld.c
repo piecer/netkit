9a9f17583dbddadfb89513eb16c07734a11acde5
piecer <piecer84@gmail.com>
2013-12-11 16:52:08 +0900

    Commit : )

--- helloworld.c
+++ helloworld.c
@@ -2,10 +2,5 @@
 int main () 
 {
 	printf("hello world \n");
-	printf("hello world \n");
-	printf("hello world \n");
-	printf("hello world \n");
-	printf("hello world \n");
-	printf("hello world \n");
 	return 0;
 }
